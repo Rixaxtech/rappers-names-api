595c596b7e6f1444350fb16bec295c5d0e3a98b6
Enrica Berko <enrica.berko@gmail.com>
2022-10-26 17:34:04 -0400

    working rapper api

---
+++ server.js
@@ -0,0 +1,39 @@
+const express = require('express')
+const app = express()
+const PORT = 8000
+
+const rappers = {
+    '21 savage': {
+        'birthName': 'Sheyaa Bin Abraham-Joseph',
+        'age': 29,
+        'birthLocation': 'London, England'
+    },
+    'chance the rapper': {
+        'birthName': 'Chancelor Bennett',
+        'age': 29,
+        'birthLocation': 'Chicago, Illinois'
+    },
+    'unknown': {
+        'birthName': 'unknown',
+        'age': 'unknown',
+        'birthLocation': 'unknown'
+    }
+}
+app.get('/', (request, response)=>{
+    response.sendFile(__dirname + '/index.html')
+})
+
+app.get('/api/:name', (request,response)=>{
+    const rapperName = request.params.name.toLowerCase()
+    if ( rappers[rapperName] ){
+        response.json(rappers[rapperName])
+    }else{
+        response.json(rappers['unknown'])
+    }
+
+})
+
+app.listen(PORT, ()=>{
+    console.log(`The server is now running on port ${PORT}! Better Catch It!`)
+})
+
